64298842062e25b3ada162b77380edceb3e18409
leedo97y <dlehdus1433@gmail.com>
2022-07-11 22:14:46 +0900

    value 찾기

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-const setNumber = document.getElementById("set-number");
-const inputSetNumber = setNumber.querySelector("input");
+const inputSetNumber = document.querySelector("#set-number input");
+
+function handleSetNumber(event) {
+  console.log(event);
+  console.log(event.target.value);
+}
+
+inputSetNumber.addEventListener("blur", handleSetNumber);
