75907b6335848f397e499dc936a257c8fdfd0428
leedo97y <dlehdus1433@gmail.com>
2022-07-12 21:39:57 +0900

    console로 you lost 출력까지 확인

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const inputSetNumber = document.querySelector("#set-number input");
 
+const guessForm = document.querySelector("#guess-number");
+const guessInput = document.querySelector("#guess-number input");
+
 function handleSetNumber(event) {
-  event.preventDefault();
+  //event.preventDefault();
   const value = event.target.value;
   console.log(value);
 }
 
+function handlePlayButton(event) {
+  event.preventDefault();
+  const guessValue = guessInput.value;
+  console.log(guessValue);
+
+  if (guessValue === inputSetNumber.value) {
+    console.log("You won!");
+  } else if (guessValue !== inputSetNumber.value) {
+    console.log("you lost!");
+  }
+}
+
 inputSetNumber.addEventListener("blur", handleSetNumber);
+guessForm.addEventListener("submit", handlePlayButton);
